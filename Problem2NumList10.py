29d4b6855875397908453ecefc61df4d6a56e928
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-12-05 06:59:51 -0600

    Update Problem2NumList10.py

--- Problem2NumList10.py
+++ Problem2NumList10.py
@@ -3,17 +3,26 @@
 # Problem #2: : create a list called L that contains the numbers 0 to 10 then create a while loop to append
 # current value then the while loop should stop once the counter variable is greater than 10.
 
-def check_nums(list):
-    i = 0
+#def check_nums(list):
+#    i = 0
+#    New_list = []
+#    while i < 10:
+#        if list[i] > 10:
+#            break
+#        New_list.append(list[i])
+#        i = i + 1
+#    return New_list
+
+def check_nums():
+    counter = 0
     New_list = []
-    while i < 10:
-        if list[i] > 10:
-            break
-        New_list.append(list[i])
-        i = i + 1
-    return New_list
+    while counter <= 10:
+        New_list.append(i)
+        counter += 1
+    print(New_list)
 
-
-L = [0,1, 2, 3, 4, 5, 6, 7, 8, 9,10]
-list = L
-print(check_nums(list))
+#L = [0,1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+#L should be created during the iteration
+#list = L: No need to change name 
+#print(check_nums(list))
+print(check_nums())
